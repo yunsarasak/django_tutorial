510e05af136975e8dae98259e54d38f296da939f
yunsarasak <rkddbstjr2@naver.com>
2024-08-09 00:55:18 +0900

    Add favicon, Image upload space

--- Yunstargram/static/modal.js
+++ Yunstargram/static/modal.js
@@ -6,10 +6,35 @@ const body = document.body;
 btnOpenModal.addEventListener("click", ()=>{
     modal.style.display="flex";
     body.style.overflow = "hidden";
+    console.log("add button click event occured")
 });
 
 
 btnCloseModal.addEventListener("click", ()=>{
+    console.log("click event occured on close button");
     modal.style.display="none";
     body.style.overflow = "auto";
 });
+
+
+$("#img_upload_space")
+    .on("click", clickEventHandler)
+    .on("dragover", dragOver)
+    .on("dragleave", dragOver)
+    .on("drop", uploadFiles);
+
+function clickEventHandler(event){
+    console.log("click event occured")
+}
+
+function dragOver(event){
+    console.log("dragover event occured");
+    event.stopPropagation();
+    event.preventDefault();
+};
+
+function uploadFiles(event){
+    console.log("drop event occured");
+    event.stopPropagation();
+    event.preventDefault();
+};
